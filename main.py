fc1d62666972f0e3b77b924eb7b3e5171f9ae783
CoopStaz <cjstaz13@gmail.com>
2024-08-01 19:59:06 +1000

    Create basic deposit and withdraw functionality

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 class Category:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.balance = 0
 
-    def deposit(self):
-        pass
+    def deposit(self, amount, description=""):
+        # Appends the amount and description to the ledger list
+        self.ledger.append({
+            'amount': amount,
+            'description': description,
+        })
 
-    def withdraw(self):
-        pass
+        # Adds to the total balance
+        self.balance += amount
+
+    def withdraw(self, amount, description=""):
+        # Return false if insufficient funds
+        if self.balance < amount:
+            return False
+
+        # If sufficient funds deduct amount from balance and append info to the ledger list
+        self.balance -= amount
+        negative_amount = -abs(amount)
+        self.ledger.append({
+            'amount': negative_amount,
+            'description': description,
+        })
+        return True
 
     def get_balance(self):
         pass
